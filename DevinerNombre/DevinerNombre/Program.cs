86e570a2235ac3cb9367927c0ed53d5591a3bef7
Valentin Vallageas <valentin.vallageas.1@etsmtl.net>
2021-09-29 18:27:31 -0400

    Création du projet "DevinerNombre"

---
+++ DevinerNombre/DevinerNombre/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevinerNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numeroADeviner = random.Next(10) + 1;
+
+            int numeroEntre;
+            //
+            //
+
+            //
+            //
+            if (!Int32.TryParse(Console.ReadLine(), out numeroEntre))
+                    Console.WriteLine("Échec de la conversion du nombre !");
+
+            //
+            //
+            //
+            //
+      
+            //
+            //
+            //
+            Console.WriteLine("Tapez n'importe quelle touche pour terminer...");
+            Console.WriteLine("---------------------------------------");
+            Console.ReadLine();
+        }
+    }
+}
