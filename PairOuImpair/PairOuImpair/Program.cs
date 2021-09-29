afac9d953dea18d967672ef49d98d0a679383bbc
Valentin Vallageas <valentin.vallageas.1@etsmtl.net>
2021-09-29 13:54:37 -0400

    Creation du projet "Pair ou Impair"

---
+++ PairOuImpair/PairOuImpair/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairOuImpair
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Déclaration de variables (entiers)
+            XXX num1, num2;
+            Console.WriteLine("Please Enter Any Number Check Even or odd.");
+
+            //Initialisation de variables
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = XXX % 2;
+
+            if (XXX == 0)
+                Console.WriteLine("\n{0} is Even Number.", XXX);
+            else
+                Console.WriteLine("\n{0} is odd Number.", XXX);
+
+            Console.ReadKey();
+        }
+    }
+}
