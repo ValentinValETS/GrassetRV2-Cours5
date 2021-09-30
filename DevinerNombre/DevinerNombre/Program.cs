569ce9b0e720edb534eca5d682f46ba49f05ec71
Valentin Vallageas <valentin.vallageas.1@etsmtl.net>
2021-09-29 23:48:49 -0400

    Ajout de commentaires

--- DevinerNombre/DevinerNombre/Program.cs
+++ DevinerNombre/DevinerNombre/Program.cs
@@ -10,15 +10,19 @@ namespace DevinerNombre
     {
         static void Main(string[] args)
         {
+            //Sortir un nombre aléatoire compris entre 1 et 10
             Random random = new Random();
             int numeroADeviner = random.Next(10) + 1;
 
+            //Initialisation du nombre que l'utilisateur va rentrer
             int numeroEntre;
             //
             //
 
             //
             //
+
+            //Récupère le nombre entré par l'utilisateur et le convertit en entier
             if (!Int32.TryParse(Console.ReadLine(), out numeroEntre))
                     Console.WriteLine("Échec de la conversion du nombre !");
 
@@ -30,6 +34,8 @@ namespace DevinerNombre
             //
             //
             //
+
+            //Une fois l'opération effectuée, message d'attente avant de fermer le programme
             Console.WriteLine("Tapez n'importe quelle touche pour terminer...");
             Console.WriteLine("---------------------------------------");
             Console.ReadLine();
